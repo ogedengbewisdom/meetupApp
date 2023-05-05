55e811d31d613e5571cacaf0fd8368fce0e55085
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-06 00:11:53 +0100

    Rendring the list of meetup

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import MeetupList from "../components/meetups/MeetupList"
+const HomePage = () => {
+    const Dummy = [
+        {
+            id: "o3",
+            image: "https://cdn.punchng.com/wp-content/uploads/2023/03/22193431/Tafawa-square-entrance.jpg",
+            title: "TimeLess Concert",
+            address: "Lagos, Nigeria"
+        },
+        {
+            id: "u3",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/69/O2_Arena.jpg",
+            title: "Raver concert",
+            address: "UK"
+        }
+    ]
+    return (
+        <MeetupList meetups={Dummy} />
+    )
+}
+
+export default HomePage
