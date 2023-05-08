75769f35f7c85a7dd2627d86aa64b0eeb1bae28a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-08 16:13:08 +0100

    dynamic ouput of data on Detail page

--- pages/[meetupId].js/index.js
+++ pages/[meetupId].js/index.js
@@ -1,8 +1,15 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
 
-// const MeetupDetailPage = () => {
-//     return (
+const MeetupDetailPage = () => {
+    return (
+        <MeetupDetail 
+        image='https://cdn.punchng.com/wp-content/uploads/2023/03/22193431/Tafawa-square-entrance.jpg'
+        title="TimeLess Concert"
+        address="Lagos, Nigeria"
+        description="love"
 
-//     )
-// }
+        />
+    )
+}
 
-// export default MeetupDetailPage
+export default MeetupDetailPage
