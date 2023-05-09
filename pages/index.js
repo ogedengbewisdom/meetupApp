42865910b18ad7cd6aea0732a9add31493ff38b4
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-09 10:15:52 +0100

    Data fetching for static pages

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,33 @@
-import { useEffect, useState } from "react"
+
 import MeetupList from "../components/meetups/MeetupList"
-const HomePage = () => {
-    const Dummy = [
-        {
-            id: "o3",
-            image: "https://cdn.punchng.com/wp-content/uploads/2023/03/22193431/Tafawa-square-entrance.jpg",
-            title: "TimeLess Concert",
-            address: "Lagos, Nigeria"
-        },
-        {
-            id: "u3",
-            image: "https://upload.wikimedia.org/wikipedia/commons/6/69/O2_Arena.jpg",
-            title: "Raver concert",
-            address: "UK"
-        }
-    ]
-    const [loadedMeetups, setLoadedMeetups] = useState([])
-    
-    useEffect( () => {
-        setLoadedMeetups(Dummy)
-    }, [])
+
+const Dummy = [
+    {
+        id: "o3",
+        image: "https://cdn.punchng.com/wp-content/uploads/2023/03/22193431/Tafawa-square-entrance.jpg",
+        title: "TimeLess Concert",
+        address: "Lagos, Nigeria"
+    },
+    {
+        id: "u3",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/69/O2_Arena.jpg",
+        title: "Raver concert",
+        address: "UK"
+    }
+]
+const HomePage = ({meetups}) => {
+
     return (
-        <MeetupList meetups={loadedMeetups} />
+        <MeetupList meetups={meetups} />
     )
+}
+
+export const getStaticProps = async () => {
+    return {
+        props: {
+            meetups : Dummy
+        }
+    }
 }
 
 export default HomePage
