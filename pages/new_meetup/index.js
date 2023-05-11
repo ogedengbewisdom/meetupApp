44db8a2fef05692023e3327b881bb716085be5a3
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-11 14:34:17 +0100

    sending http request to api routes

--- pages/new_meetup/index.js
+++ pages/new_meetup/index.js
@@ -1,8 +1,19 @@
-import NewMeetupForm from "../../components/meetups/NewMeetupForm"
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetupPage = () => {
-    const addMeetupHandler = (metaData) => {
-        console.log(metaData)
+    const router = useRouter();
+    const addMeetupHandler = async (metaData) => {
+        const response = await fetch("/api/new_meetup", {
+            method: "POST",
+            body: JSON.stringify(metaData),
+            headers: {
+                "Content-Type" : "application/json"
+            }
+        })
+        const data = await response.json();
+        console.log(data);
+        router.push("/")
     }
     return (
         <NewMeetupForm onAddMeetup={addMeetupHandler} />
