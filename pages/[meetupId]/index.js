1abc82e0ef861a7a541866258213c2f71aaa1373
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-05-12 13:46:13 +0100

    fetching dynamic data base on id

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,4 +1,5 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail"
+import { MongoClient, ObjectId } from "mongodb"
 
 
 const MeetupDetailPage = ({meetups}) => {
@@ -9,35 +10,41 @@ const MeetupDetailPage = ({meetups}) => {
 }
 
 export const getStaticPaths = async () => {
+    const url = `mongodb+srv://ogedengbewisdom1994:Test123456@cluster0.lqyedd0.mongodb.net/meetups?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(url);
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+    const meetups = await meetupCollection.find({}, {_id: 1}).toArray()
+    client.close()
     return {
         fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: "o3"
-                }
-            },
-            {
-                params: {
-                    meetupId: "u3"
-                }
+        paths: meetups.map(meetup => ({
+            params: {
+                meetupId: meetup._id.toString()
             }
-        ]
+        }))
+        
     }
 }
 
 
 export const getStaticProps = async (context) => {
     const meetupId = context.params.meetupId
-    console.log(meetupId)
+    const url = `mongodb+srv://ogedengbewisdom1994:Test123456@cluster0.lqyedd0.mongodb.net/meetups?retryWrites=true&w=majority`;
+    const client = await MongoClient.connect(url);
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+    const selectedMeetup = await meetupCollection.findOne({_id: ObjectId(meetupId)})
+    client.close()
+    console.log(meetupId);
     return {
         props: {
             meetups: {
-                id: meetupId,
-                title: "TimeLess Concert",
-                address : "TBS, Lagos, Nigeria",
-                image : "https://cdn.punchng.com/wp-content/uploads/2023/03/22193431/Tafawa-square-entrance.jpg",
-                description: "The best Place to be at"
+                id: selectedMeetup.meetupId.toString(),
+                title: selectedMeetup.title,
+                address : selectedMeetup.address,
+                image : selectedMeetup.image,
+                description: selectedMeetup.description
             }
         }
     }
